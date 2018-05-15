2c2d865b262441c5d089f2013aab4162a0b34c0f
Pacho Salazar <jfsh87@hotmail.com>
2018-05-15 19:55:34 -0400

    html routing and server set up

---
+++ app/server.js
@@ -0,0 +1,24 @@
+
+// packages required
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+// information required by database
+var friend = require("./data/friends.js");
+//express and port 
+var app = express();
+var PORT = process.env.PORT || 8080; 
+// static content with out it it will not work
+app.use(express.static("app/public"));
+//express
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// Routes
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+//listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
